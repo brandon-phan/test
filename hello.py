3ddfc6b0f4b8a45590e5aaca4b21585e1033e429
Brandon Phan <bphan@laptop-computer.home>
2019-09-29 11:31:52 -0400

    module 5 lessons learned

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello World!")
 print("This commit is to trigger a Jenkins build.")
 print("Here is another commit to prove functionality of Jenkins.")
+print("In Module 5 I integrated jenkins and github to begin CI/CD")
